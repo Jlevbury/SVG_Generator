1630dc224dde77d7d20fc3ff35cf1406dcfe0537
Jlevbury <125767916+Jlevbury@users.noreply.github.com>
2023-05-22 14:39:38 -0400

    updated formatting, working on validation of input function

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
 const questions = [
-    {type: 'input', name: 'initials', message: 'Please enter up to three letters'},
-    {type: 'input', name: 'text-color', message: 'Please enter a color choice or hexidecimal color code'},
-    {type: 'list', name: 'shape-selector', message: 'Please choose one of the available shapes', choices: ['Square', 'Triangle', 'Circle'] },
-    {type: 'input', name: 'shape-color', message: 'Please enter a color choice or hexidecimal color code'}
-    ];
+	{
+		type: "input",
+		name: "initials",
+		message: "Please enter up to three letters",
+	},
+	{
+		type: "input",
+		name: "text-color",
+		message: "Please enter a color choice or hexidecimal color code",
+	},
+	{
+		type: "list",
+		name: "shape-selector",
+		message: "Please choose one of the available shapes",
+		choices: ["Square", "Triangle", "Circle"],
+	},
+	{
+		type: "input",
+		name: "shape-color",
+		message: "Please enter a color choice or hexidecimal color code",
+	},
+];
 
 function init() {
-    inquirer.prompt(questions).then((answers) => {
-        
-    })
+	inquirer.prompt(questions).then((answers) => {});
 }
